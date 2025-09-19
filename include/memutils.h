f8a10fb870d66b5925c92ce342cd301fa6ee1044
umarnurmatov <umarkhonnur12@gmail.com>
2025-09-19 19:58:05 +0300

    #FIXED nfree

--- include/memutils.h
+++ include/memutils.h
@@ -2,11 +2,5 @@
 
 #include <stdlib.h>
 
-/// @brief   free and null
-/// @details free memory pointed by \p ptr and assigng NULL
-/// @param   ptr pointer to free
-inline void nfree(void* ptr)
-{
-    free(ptr);
-    ptr = nullptr;
-}
+#define NFREE(ptr) free(ptr); \
+                   ptr = NULL;
