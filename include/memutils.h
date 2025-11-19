262f1813c003726babfe2923e9ddadfaf251506e
umarnurmatov <umarkhonnur12@gmail.com>
2025-11-19 23:26:10 +0300

    #FIXED put NFREE body into separate scope

--- include/memutils.h
+++ include/memutils.h
@@ -2,8 +2,11 @@
 
 #include <stdlib.h>
 
-#define NFREE(ptr) free(ptr); \
-                   ptr = NULL;
+#define NFREE(ptr) \
+    {              \
+       free(ptr);  \
+       ptr = NULL; \
+    }
 
 #define TYPED_CALLOC(size, type) \
     (type*) calloc (size, sizeof(type))
