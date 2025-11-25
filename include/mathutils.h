74515629c54d3eec00ab458a7c0cb0558142e1dc
umarnurmatov <umarkhonnur12@gmail.com>
2025-11-25 16:13:15 +0300

    #MODIFIED made mathutils funcs uninlined

--- include/mathutils.h
+++ include/mathutils.h
@@ -1,5 +1,5 @@
 #pragma once
 
-inline int utils_imax(int a, int b);
+int utils_imax(int a, int b);
 
-inline int utils_imin(int a, int b);
+int utils_imin(int a, int b);
--- src/mathutils.c
+++ src/mathutils.c
@@ -1,11 +1,11 @@
 #include "mathutils.h"
 
-inline int utils_imax(int a, int b)
+int utils_imax(int a, int b)
 {
     return a > b ? a : b;
 }
 
-inline int utils_imin(int a, int b)
+int utils_imin(int a, int b)
 {
     return a < b ? a : b;
 }
