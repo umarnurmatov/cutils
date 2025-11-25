fc9e8965a26da5338142ad938a0292462c7d75e8
umarnurmatov <umarkhonnur12@gmail.com>
2025-11-25 15:37:13 +0300

    #ADDED demangling funcs to assertutils
    they aren't working

--- include/assertutils.h
+++ include/assertutils.h
@@ -13,9 +13,6 @@
 
 #define verified(expr) || ({ expr; false; })
 
-/// @brief prints stack trace to stderr 
-void utils_print_stacktrace(void);
-
 /// @brief prints failed assertion info  and stack trace to stderr (see utils_assert macro)
 /// @param expression string containing asserted expression
 /// @param filename filename where assertion occured
--- src/assertutils.c
+++ src/assertutils.c
@@ -1,26 +1,65 @@
 #include "assertutils.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <execinfo.h>
+#include <dlfcn.h>
+#include <cxxabi.h>
 
 #include "colorutils.h"
 
 static const int MAX_STACKTRACE_SIZE = 256;
 
-void utils_print_stacktrace(void)
+static char* utils_demangle_backtrace_string_cxx_(char* str);
+static const char* utils_demangle_backtrace_string_c_(char* str);
+static void utils_print_stacktrace_(void);
+
+static char* utils_demangle_backtrace_string_cxx_(char* str)
+{
+    char* symbol_begin = strchr(str, '(') + 1;
+    char* symbol_end = strchr(str, '+');
+
+    int status = 0;
+
+    *symbol_end = '\0';
+    char* realname = __cxxabiv1::__cxa_demangle(symbol_begin, NULL, NULL, &status);
+    *symbol_end = '+';
+    
+    fprintf(stderr, "%d\n", status);
+
+    if(status != 0)
+        return NULL;
+
+    return realname;
+}
+
+#define BUF_SIZE 100
+static const char* utils_demangle_backtrace_string_c_(char* str)
+{
+    static Dl_info info;
+    static char buffer[BUF_SIZE];
+
+    if(dladdr(str, &info))
+        return info.dli_sname;
+
+    return str;
+}
+
+static void utils_print_stacktrace_(void)
 {
     void** backtrace_buffer = (void**)calloc(MAX_STACKTRACE_SIZE, sizeof(void*));
 
     int backtrace_size = backtrace(backtrace_buffer, MAX_STACKTRACE_SIZE);
-    char** backtracce_strings = backtrace_symbols(backtrace_buffer, backtrace_size);
-    if (backtracce_strings != NULL)
+    char** backtrace_strings = backtrace_symbols(backtrace_buffer, backtrace_size);
+    if (backtrace_strings != NULL)
     {
-        for(int i = 0; i < backtrace_size; ++i)
-            utils_colored_fprintf(stderr, ANSI_COLOR_YELLOW, "%s\n", backtracce_strings[i]);
+        for(int i = 0; i < backtrace_size; ++i) {
+            utils_colored_fprintf(stderr, ANSI_COLOR_YELLOW, "%s\n", utils_demangle_backtrace_string_c_(backtrace_strings[i]));
+        }
     }
 
-    free(backtracce_strings);
+    free(backtrace_strings);
     free(backtrace_buffer);
 }
 
@@ -28,6 +67,6 @@ void utils_assert_fail(const char *expression, const char *filename, int linenum
 {
     utils_colored_fprintf(stderr, ANSI_COLOR_BOLD_WHITE, "%s:%d: assertion failed\n", filename, linenumber);
     utils_colored_fprintf(stderr, ANSI_COLOR_BOLD_RED, "   assert(%s)\n", expression);
-    utils_print_stacktrace();
+    utils_print_stacktrace_();
     abort();
 }
