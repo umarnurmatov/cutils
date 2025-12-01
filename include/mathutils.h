3aea1337581907fd0e7ce0832b5b8d336e1e9188
umarnurmatov <umarkhonnur12@gmail.com>
2025-12-01 16:24:12 +0300

    #ADDED utils_factorial

--- include/mathutils.h
+++ include/mathutils.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <stdint.h>
 
 int utils_imax(int a, int b);
 
 int utils_imin(int a, int b);
+
+uint64_t utils_i64_factorial(uint64_t x);
--- src/mathutils.c
+++ src/mathutils.c
@@ -9,3 +9,12 @@ int utils_imin(int a, int b)
 {
     return a < b ? a : b;
 }
+
+uint64_t utils_i64_factorial(uint64_t x)
+{
+    uint64_t ans = 1;
+    for(uint64_t i = 2; i <= x; ++i)
+        ans *= i; 
+
+    return ans;
+    }
